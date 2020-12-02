7e77e44c209a9f880f5b53d288c36f448a4f8f84
Kthiessen <kurtissen@gmail.com>
2020-12-02 18:36:26 -0500

    Added images

--- Square.cs
+++ Square.cs
@@ -4,36 +4,31 @@ using System;
 public class Square : Godot.Node2D
 {
     private Rect2 boundingBox;
-    private Polygon2D poly;
 
     public int selectedByPlayer { get; private set; } = -1;
+    public Texture[] playerTextures;
 
     [Signal]
     public delegate void Clicked(Square target);
 
     public override void _Ready()
     {
-        poly = GetNode<Polygon2D>("Polygon2D");
-
-        Vector2 minPoint = new Vector2(float.MaxValue, float.MaxValue);
-        Vector2 maxPoint = new Vector2(float.MinValue, float.MinValue);
-
-        foreach (Vector2 vertex in poly.Polygon)
-        {
-            minPoint.x = Math.Min(vertex.x, minPoint.x);
-            minPoint.y = Math.Min(vertex.y, minPoint.y);
-            maxPoint.x = Math.Max(vertex.x, maxPoint.x);
-            maxPoint.y = Math.Max(vertex.y, maxPoint.y);
-        }
-
-        minPoint.x *= poly.Scale.x;
-        minPoint.y *= poly.Scale.y;
-        maxPoint.x *= poly.Scale.x;
-        maxPoint.y *= poly.Scale.y;
-
-        boundingBox = new Rect2(ToGlobal(minPoint), maxPoint - minPoint);
+        PreloadTextures();
+        GetBoundingBox();
     }
 
+    private void PreloadTextures()
+    {
+        playerTextures = new Texture[] {
+            GD.Load<Texture>("res://assets/X.png"),
+            GD.Load<Texture>("res://assets/O.png")
+        };
+    }
+
+    private void GetBoundingBox()
+    {
+        boundingBox = new Rect2(ToGlobal(new Vector2(-50, -50)), new Vector2(100, 100));
+    }
     public override void _Input(InputEvent inputEvent)
     {
         base._Input(inputEvent);
@@ -58,14 +53,7 @@ public class Square : Godot.Node2D
     {
         if (selectedByPlayer < 0)
         {
-            if (playerTurn == 0)
-            {
-                GetNode<Polygon2D>("Polygon2D").Color = new Color(0, 255, 0);
-            }
-            else
-            {
-                GetNode<Polygon2D>("Polygon2D").Color = new Color(255, 0, 0);
-            }
+            GetNode<Sprite>("Sprite").Texture = playerTextures[playerTurn];
             selectedByPlayer = playerTurn;
         }
     }
